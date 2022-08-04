87861aa37e309a7d4ff4682d37aac15e8f877b1a
Barış Demirhan <barisdemirhn@gmail.com>
2022-08-04 19:56:24 +0300

    bug fix

--- src/Database/Migrations/2022_08_04_094501_update_quantity_type_to_basket_items_table.php
+++ src/Database/Migrations/2022_08_04_094501_update_quantity_type_to_basket_items_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class AddUpdateQuantityTypeToBasketItemsTable extends Migration
+class updateQuantityTypeToBasketItemsTable extends Migration
 {
     /**
      * Run the migrations.
